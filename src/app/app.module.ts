427517c7a47ae03c52a1369572f0c1a2d495680e
Nachib soukaina <soukaina.nachib23@gmail.com>
2020-03-09 15:03:34 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPostComponent } from './components/md-post/add-post/add-post.component';
+import { EditPostComponent } from './components/md-post/edit-post/edit-post.component';
+import { ShowPostComponent } from './components/md-post/show-post/show-post.component';
+import { ListPostComponent } from './components/md-post/list-post/list-post.component';
+import { ListCategoryComponent } from './components/md-category/list-category/list-category.component';
+import { AddCategoryComponent } from './components/md-category/add-category/add-category.component';
+import { EditCategoryComponent } from './components/md-category/edit-category/edit-category.component';
+import { ShowCategoryComponent } from './components/md-category/show-category/show-category.component';
+import { ShowUserComponent } from './components/md-user/show-user/show-user.component';
+import { AddUserComponent } from './components/md-user/add-user/add-user.component';
+import { EditUserComponent } from './components/md-user/edit-user/edit-user.component';
+import { ListUserComponent } from './components/md-user/list-user/list-user.component';
+import { ListCommentComponent } from './components/md-comment/list-comment/list-comment.component';
+import { AddCommentComponent } from './components/md-comment/add-comment/add-comment.component';
+import { EditCommentComponent } from './components/md-comment/edit-comment/edit-comment.component';
+import { ShowCommentComponent } from './components/md-comment/show-comment/show-comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPostComponent,
+    EditPostComponent,
+    ShowPostComponent,
+    ListPostComponent,
+    ListCategoryComponent,
+    AddCategoryComponent,
+    EditCategoryComponent,
+    ShowCategoryComponent,
+    ShowUserComponent,
+    AddUserComponent,
+    EditUserComponent,
+    ListUserComponent,
+    ListCommentComponent,
+    AddCommentComponent,
+    EditCommentComponent,
+    ShowCommentComponent
   ],
   imports: [
     BrowserModule,
